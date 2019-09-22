aefe20f00a26b810a6337bd2e65beb2a2de1a0b9
Jonathanpatta <49340189+Jonathanpatta@users.noreply.github.com>
2019-09-22 14:51:22 +0530

    Update tkclass.py

--- tkclass.py
+++ tkclass.py
@@ -1,11 +1,12 @@
 import tkinter as tk
 import mysql.connector as sql
+import datetime
 
 #import functions
 
 
 class tkframe:
-    def __init__(self,master):
+    def __init__(self,master,text):
         # ***** db call *****
         self.mydb = sql.connect(
                         host = "localhost",
@@ -34,21 +35,20 @@ class tkframe:
         self.frame.place(relx=0.5,rely=0.1,relwidth =0.8,relheight = 0.07,anchor='n')
 
         self.entry = tk.Entry(self.frame,bg = '#ECF0F1')
-        self.entry.place(relx=0.2,relwidth=0.5,relheight=1)
+        self.entry.place(relwidth=0.75,relheight=1)
 
         
 
-        self.button = tk.Button(self.frame, text = "click to enter client record",bg = '#ECF0F1',fg = 'red',command=lambda: self.show_db())#self.insert_clients(self.entry.get()))
-        self.button.place(relx=0.75,relwidth=0.25,relheight=1)
+        self.button = tk.Button(self.frame, text = "submit",bg = '#ECF0F1',fg = 'red',command=lambda: self.func_call(text,self.entry.get()))
+        self.button.place(relx=0.80,relwidth=0.2,relheight=1)
 
-        self.lab = tk.Label(self.frame,text ="text label",bg = '#ECF0F1',fg='red')
-        self.lab.place(relwidth=0.15,relheight=1)
+        
         
 
         # ****** lower textbox ******
 
         self.lower_frame = tk.Frame(master, bg='#2ECC71', bd=10)
-        self.lower_frame.place(relx=0.5, rely=0.6, relwidth=0.75, relheight=0.25, anchor='n')
+        self.lower_frame.place(relx=0.5, rely=0.2, relwidth=0.8, relheight=0.75, anchor='n')
 
 
 
@@ -63,6 +63,24 @@ class tkframe:
 
     # ************************************** DBMS call functions *************************************************
 
+    def func_call(self,text,entry):
+        if(text=="user_insert"):
+            self.insert_users(entry)
+        if(text=="booking_insert"):
+            self.insert_bookings(entry)
+        if(text=="room_insert"):
+            self.insert_rooms(entry)
+        if(text=="usertype_insert"):
+            self.insert_usertype(entry)
+
+        if(text=="user_read"):
+            self.read_users()
+        if(text=="booking_read"):
+            self.read_bookings()
+        if(text=="room_read"):
+            self.read_rooms()
+        if(text=="usertype_read"):
+            self.read_usertype()
 
     def connect_db(self):
             self.mydb = sql.connect(
@@ -81,19 +99,120 @@ class tkframe:
         for db in self.mycursor:
             entry=entry+"\n"+db[0]
         self.show_label(entry)
+#************************************users table****************************************************
+    def insert_users(self,entry):
+        fname,lname,phno,company,usertype,loginid,password=entry.split(',')
 
-    def insert_clients(self,entry):
-        tup = tuple(entry)
-        print(entry)
-        client="INSERT INTO client (clientid,fname,lname,phoneno,company) VALUES (%s, %s, %s, %s, %s)"
-
-        clid=self.entry.get()
-        print(tup)
-        #self.mycursor.execute(client,tup)
-        self.get_text(entry)
+        
+        tup=(fname,lname,phno,company,usertype,loginid,password)
+        client="INSERT INTO users (fname,lname,phoneno,company,usertype,loginid,password) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        self.mycursor.execute(client, tup)
+        self.show_label(entry)
+        self.read_users()
         #self.mydb.commit()
 
-    def get_text(self,entry):
-        self.show_label( entry + '\nhas been added to the table' )
+
+    def read_users(self):
+        self.mycursor.execute("SELECT * FROM `users`")
+        fetch = self.mycursor.fetchall()
+        print(fetch[0][0])
+        p=""
+        for data in fetch:
+            for data1 in data:
+                p+=str(data1)+","
+            p+="\n"
+        self.show_label(p)
+
+#************************************bookings table****************************************************
+    def insert_bookings(self,entry):
+        username,password,start_year,start_month,start_day,end_year,end_month,end_day,room_name=entry.split(',')
+        
+        query = ("SELECT password,loginid,userid FROM users WHERE password = %s AND loginid = %s")
+        self.mycursor.execute(query,(password,username))
+        fetch=self.mycursor.fetchall()
+        if len(fetch)!=1:
+            self.show_label("access denied!!")
+
+        else:
+            query1 = ("SELECT roomid FROM rooms WHERE roomname = %s")
+            self.mycursor.execute(query1,(room_name,))
+            fetch1=self.mycursor.fetchall()
+            if len(fetch1)!=1:
+                self.show_label("room not found.selecting default room")
+                roomid=2
+            else:
+                roomid=fetch1[0][0]
+            description="nice!"
+            startdt=datetime.datetime(int(start_year),int(start_month),int(start_day),9,0,0,0)
+            enddt=datetime.datetime(int(end_year),int(end_month),int(end_day),21,0,0,0)
+            createdt=datetime.datetime.now()
+            modifydt=datetime.datetime.now()
+            userid=fetch[0][2]
+            tup=(startdt,enddt,description,userid,roomid,createdt,modifydt)
+            client="INSERT INTO bookings (startdate,enddate,description,userid,roomid,createddate,modifieddate) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+            self.mycursor.execute(client, tup)
+            self.show_label(entry)
+            #self.mydb.commit()
         
 
+
+    def read_bookings(self):
+        self.mycursor.execute("SELECT * FROM `bookings`")
+        fetch = self.mycursor.fetchall()
+        p=""
+        for data in fetch:
+            p=p+data+"\n"
+        self.show_label(p)
+
+#************************************rooms table****************************************************
+    def insert_rooms(self,entry):
+        username,password,room_name,seats,room_area,price_seat,price_sqft=entry.split(',')
+        
+        query = ("SELECT password,loginid,userid,fname FROM users WHERE password = %s AND loginid = %s")
+        self.mycursor.execute(query,(password,username))
+        fetch=self.mycursor.fetchall()
+        if len(fetch)!=1:
+            self.show_label("access denied!!")
+
+        else:
+            description="nice!"
+            createdt=datetime.datetime.now()
+            modifydt=datetime.datetime.now()
+            userid=fetch[0][2]
+            createdby=fetch[0][3]
+            tup=(room_name,seats,room_area,price_seat,price_sqft,description,createdt,modifydt,createdby)
+            client="INSERT INTO rooms (roomname,seats,roomarea,priceseat,pricesqft,description,createddate,modifieddate,createdby) VALUES (%s, %s, %s, %s, %s, %s, %s, %s. %s)"
+            self.mycursor.execute(client, tup)
+            self.show_label(entry)
+            #self.mydb.commit()
+        
+
+
+    def read_rooms(self):
+        self.mycursor.execute("SELECT * FROM `rooms`")
+        fetch = self.mycursor.fetchall()
+        p=""
+        for data in fetch:
+            p=p+data+"\n"
+        self.show_label(p)
+
+#************************************usertype table****************************************************
+    def insert_usertype(self,entry):
+        fname,lname,phno,company,usertype,loginid,password=entry.split(',')
+
+        
+        tup=(fname,lname,phno,company,usertype,loginid,password)
+        client="INSERT INTO users (fname,lname,phoneno,company,usertype,loginid,password) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        self.mycursor.execute(client, tup)
+        self.show_label(entry)
+        self.read_users()
+        #self.mydb.commit()
+
+
+    def read_usertype(self):
+        self.mycursor.execute("SELECT * FROM `usertype`")
+        fetch = self.mycursor.fetchall()
+        p=""
+        for data in fetch:
+            p=p+data+"\n"
+        self.show_label(p)
